29fc41a07ce7e001cdc8e1e1f8252285dc7edafd
oxalica <oxalicc@pm.me>
2020-05-03 00:45:14 +0800

    Move inode pool to a new mod

---
+++ src/fs/vfs/inode.rs
@@ -0,0 +1,105 @@
+use fuse::FUSE_ROOT_ID;
+use onedrive_api::ItemId;
+use sharded_slab::{Clear, Pool};
+use std::{
+    collections::hash_map::{Entry, HashMap},
+    sync::atomic::{AtomicU64, Ordering},
+};
+use time::Timespec;
+use tokio::sync::Mutex;
+
+// This should not hold any heap-allocation due to the requirement `Inode: Clear`.
+pub struct InodeAttr {
+    pub size: u64,
+    pub mtime: Timespec,
+    pub crtime: Timespec,
+    pub is_directory: bool,
+}
+
+#[derive(Default)]
+pub struct InodePool {
+    pool: Pool<Inode>,
+    rev_map: Mutex<HashMap<ItemId, usize>>,
+}
+
+struct Inode {
+    ref_count: AtomicU64,
+    item_id: ItemId,
+}
+
+impl Clear for Inode {
+    fn clear(&mut self) {
+        self.item_id.0.clear();
+    }
+}
+
+// Required by `Pool`. Set to an invalid state.
+impl Default for Inode {
+    fn default() -> Self {
+        Self {
+            ref_count: 0.into(),
+            item_id: ItemId(String::new()),
+        }
+    }
+}
+
+impl Inode {
+    fn new(item_id: ItemId) -> Self {
+        Self {
+            ref_count: 1.into(),
+            item_id,
+        }
+    }
+}
+
+impl InodePool {
+    fn idx_to_ino(idx: usize) -> u64 {
+        (idx as u64).wrapping_add(FUSE_ROOT_ID + 1)
+    }
+
+    fn ino_to_idx(ino: u64) -> usize {
+        assert_ne!(ino, FUSE_ROOT_ID);
+        ino.wrapping_sub(FUSE_ROOT_ID + 1) as usize
+    }
+
+    pub async fn get_or_alloc_ino(&self, item_id: ItemId) -> u64 {
+        let idx = match self.rev_map.lock().await.entry(item_id) {
+            Entry::Occupied(ent) => {
+                let idx = *ent.get();
+                self.pool
+                    .get(idx)
+                    .unwrap()
+                    .ref_count
+                    .fetch_add(1, Ordering::Relaxed);
+                idx
+            }
+            Entry::Vacant(ent) => {
+                let idx = self
+                    .pool
+                    .create(|p| *p = Inode::new(ent.key().clone()))
+                    .expect("Pool is full");
+                ent.insert(idx);
+                idx
+            }
+        };
+        Self::idx_to_ino(idx)
+    }
+
+    pub fn get_item_id(&self, ino: u64) -> Option<ItemId> {
+        Some(self.pool.get(Self::ino_to_idx(ino))?.item_id.clone())
+    }
+
+    pub async fn free(&self, ino: u64, count: u64) -> Option<()> {
+        let idx = Self::ino_to_idx(ino);
+        // Lock first to avoid race with get_or_alloc.
+        let mut rev_g = self.rev_map.lock().await;
+        let g = self.pool.get(idx)?;
+        let orig_ref_count = g.ref_count.fetch_sub(count, Ordering::Relaxed);
+        if count < orig_ref_count {
+            return Some(());
+        }
+        assert!(rev_g.remove(&g.item_id).is_some());
+        assert!(self.pool.clear(idx));
+        Some(())
+    }
+}
--- src/fs/vfs/mod.rs
+++ src/fs/vfs/mod.rs
@@ -3,15 +3,11 @@ use anyhow::Result as AResult;
 use fuse::FUSE_ROOT_ID;
 use onedrive_api::{FileName, ItemId, ItemLocation, OneDrive};
 use reqwest::StatusCode;
-use sharded_slab::{Clear, Pool};
-use std::{
-    collections::hash_map::{Entry, HashMap},
-    ffi::OsStr,
-    sync::atomic::{AtomicU64, Ordering},
-    time::Duration,
-};
+use std::{ffi::OsStr, time::Duration};
 use time::Timespec;
-use tokio::sync::Mutex;
+
+mod inode;
+pub use inode::InodeAttr;
 
 trait ResultExt<T> {
     fn c_err(self, target: &'static str, errno: libc::c_int) -> CResult<T>;
@@ -56,7 +52,7 @@ impl<'a> Into<ItemLocation<'a>> for &'a OwnedItemLocation {
 
 #[derive(Default)]
 pub struct Vfs {
-    inode_pool: InodePool,
+    inode_pool: inode::InodePool,
 }
 
 impl Vfs {
@@ -191,101 +187,5 @@ impl Vfs {
             is_directory: item.folder.is_some(),
         };
         Ok(Some((item_id, attr)))
-    }
-}
-
-// This should not hold any heap-allocation due to the requirement `Inode: Clear`.
-pub struct InodeAttr {
-    pub size: u64,
-    pub mtime: Timespec,
-    pub crtime: Timespec,
-    pub is_directory: bool,
-}
-
-#[derive(Default)]
-struct InodePool {
-    pool: Pool<Inode>,
-    rev_map: Mutex<HashMap<ItemId, usize>>,
-}
-
-struct Inode {
-    ref_count: AtomicU64,
-    item_id: ItemId,
-}
-
-impl Clear for Inode {
-    fn clear(&mut self) {
-        self.item_id.0.clear();
-    }
-}
-
-// Required by `Pool`. Set to an invalid state.
-impl Default for Inode {
-    fn default() -> Self {
-        Self {
-            ref_count: 0.into(),
-            item_id: ItemId(String::new()),
-        }
-    }
-}
-
-impl Inode {
-    fn new(item_id: ItemId) -> Self {
-        Self {
-            ref_count: 1.into(),
-            item_id,
-        }
-    }
-}
-
-impl InodePool {
-    fn idx_to_ino(idx: usize) -> u64 {
-        (idx as u64).wrapping_add(FUSE_ROOT_ID + 1)
-    }
-
-    fn ino_to_idx(ino: u64) -> usize {
-        assert_ne!(ino, FUSE_ROOT_ID);
-        ino.wrapping_sub(FUSE_ROOT_ID + 1) as usize
-    }
-
-    async fn get_or_alloc_ino(&self, item_id: ItemId) -> u64 {
-        let idx = match self.rev_map.lock().await.entry(item_id) {
-            Entry::Occupied(ent) => {
-                let idx = *ent.get();
-                self.pool
-                    .get(idx)
-                    .unwrap()
-                    .ref_count
-                    .fetch_add(1, Ordering::Relaxed);
-                idx
-            }
-            Entry::Vacant(ent) => {
-                let idx = self
-                    .pool
-                    .create(|p| *p = Inode::new(ent.key().clone()))
-                    .expect("Pool is full");
-                ent.insert(idx);
-                idx
-            }
-        };
-        Self::idx_to_ino(idx)
-    }
-
-    fn get_item_id(&self, ino: u64) -> Option<ItemId> {
-        Some(self.pool.get(Self::ino_to_idx(ino))?.item_id.clone())
-    }
-
-    async fn free(&self, ino: u64, count: u64) -> Option<()> {
-        let idx = Self::ino_to_idx(ino);
-        // Lock first to avoid race with get_or_alloc.
-        let mut rev_g = self.rev_map.lock().await;
-        let g = self.pool.get(idx)?;
-        let orig_ref_count = g.ref_count.fetch_sub(count, Ordering::Relaxed);
-        if count < orig_ref_count {
-            return Some(());
-        }
-        assert!(rev_g.remove(&g.item_id).is_some());
-        assert!(self.pool.clear(idx));
-        Some(())
     }
 }
