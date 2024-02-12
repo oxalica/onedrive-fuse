bc5d470d4cab865f3cbd46cf4daec6a5ef7e8574
oxalica <oxalicc@pm.me>
2024-02-12 02:09:49 -0500

    Refactor and update flake

--- flake.nix
+++ flake.nix
@@ -2,31 +2,26 @@ rec {
   description = "Mount Microsoft OneDrive storage as FUSE filesystem";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.flake-utils.follows = "flake-utils";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         inherit (nixpkgs) lib;
         pkgs = nixpkgs.legacyPackages.${system};
         manifest = lib.importTOML (self + "/Cargo.toml");
 
-        inherit (manifest.package) name version;
-
-        nativeBuildInputs = with pkgs; [ pkg-config ];
-        buildInputs = with pkgs; [ fuse3 openssl ];
-
       in {
         packages = rec {
-          ${name} = default;
+          ${default.name} = default;
           default = pkgs.rustPlatform.buildRustPackage {
-            pname = name;
-            inherit version nativeBuildInputs buildInputs;
+            pname = manifest.package.name;
+            version = manifest.package.version;
+
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            buildInputs = with pkgs; [ fuse3 openssl ];
 
             src = self;
             cargoLock.lockFile = self + "/Cargo.lock";
@@ -39,13 +34,19 @@ rec {
           };
         };
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = nativeBuildInputs ++ [
-            rust-overlay.packages.${system}.rust
-          ];
-          inherit buildInputs;
+        devShells = rec {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
 
-          RUST_BACKTRACE = 1;
+            RUST_BACKTRACE = 1;
+          };
+
+          without-rust = default.overrideAttrs (old: {
+            nativeBuildInputs =
+              lib.filter
+                (drv: builtins.match ".*(rustc|cargo).*" drv.name == null)
+                old.nativeBuildInputs;
+          });
         };
       });
 }
